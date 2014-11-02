5e67c3d8b9af61732d7d9c124ac9b1d5ffa67cf2
Duncan Fermor <duncan.fermor@gmail.com>
2014-11-02 16:09:00 +0000

    guard against pages being set to 0 and increased page size to 10

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class App
   get '/' do
-    page_size = 2
+    page_size = 10
     params[:page] = (Transaction.all.count / page_size.to_f).ceil unless params[:page]
+    params[:page] = 1 if params[:page] < 1
     @transactions = Transaction.paginate(page: params[:page], per_page: page_size).table_order
     @categories = Category.all.order('column')
     @total = Transaction.all.inject(0) { |total, transaction| transaction.amount + total }
