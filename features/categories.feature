e052924947f1d8aa227377102dcb646e19805bf1
Duncan Fermor <duncan@elitemail.org>
2014-12-23 14:12:49 +0000

    failing feature for creating a category

--- features/categories.feature
+++ features/categories.feature
@@ -5,3 +5,11 @@ Feature: CRUD categories
     When  I visit the homepage
     And   click the categories link
     Then  I see the list of categories
+
+  @wip
+  Scenario: creating a new category
+    When I visit the homepage
+    And click the categories link
+    And click the New Category link
+    And fill out the New Category form
+    Then I see the new category on the homepage
--- features/step_definitions/categories_steps.rb
+++ features/step_definitions/categories_steps.rb
@@ -6,3 +6,18 @@ Then 'I see the list of categories' do
   expect(page.body).to have_content('category 1')
   expect(page.body).to have_content('category 2')
 end
+
+When 'click the New Category link' do
+  click_link 'New Category'
+end
+
+When 'fill out the New Category form' do
+  fill_in 'Name', with: 'groceries'
+
+end
+
+Then 'I see the new category on the homepage' do
+  expect(page.body).to have_content 'groceries'
+end
+
+
