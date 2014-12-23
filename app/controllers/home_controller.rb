d2e62ca77f7265e428f2587ac71ccd1942757649
Duncan Fermor <duncan@elitemail.org>
2014-12-23 23:08:31 +0000

    bootstrap!!!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class App
     page_size = 10
     params[:page] = (Transaction.all.count / page_size.to_f).ceil unless params[:page]
     params[:page] = 1 if (params[:page].to_i) < 1
+    @page_number = params[:page].to_i
     @transactions = Transaction.paginate(page: params[:page], per_page: page_size).table_order
     @categories = Category.all
     @total = Transaction.all.inject(0) { |total, transaction| transaction.amount + total }
